77f67ef9830f285cfa71e0caea37249d2750178b
Andrey <AKuropatkin@egartech.ru>
2024-11-11 21:09:42 +0300

    fix/cors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  app.enableShutdownHooks()
+
+  app.enableShutdownHooks();
   app.setGlobalPrefix('api');
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:4173', 'https://remostar.vercel.app'],
+  });
 
   await app.listen(5000);
 }
